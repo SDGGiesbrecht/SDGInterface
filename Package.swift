53ed1a7aebd12186497154aa60d8ffb4dfdb6c9a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-06 15:33:41 -0600

    Documented package.

--- Package.swift
+++ Package.swift
@@ -16,14 +16,35 @@
 
 import PackageDescription
 
+/// SDGInterface provides tools for implementing a graphical user interface.
+///
+/// > [Καὶ ὁ Λόγος σὰρξ ἐγένετο καὶ ἐσκήνωσεν ἐν ἡμῖν, καὶ ἐθεασάμεθα τὴν δόξαν αὐτοῦ, δόξαν ὡς μονογενοῦς παρὰ πατρός, πλήρης χάριτος καὶ ἀληθείας.](https://www.biblegateway.com/passage/?search=John+1&version=SBLGNT;NIV)
+/// >
+/// > [And the Word became flesh and dwelt among us and we have watched His glory, the glory of the Only Begotten of the Father, full of grace and truth.](https://www.biblegateway.com/passage/?search=John+1&version=SBLGNT;NIV)
+/// >
+/// > ―‎יוחנן⁩/Yoẖanan
+///
+/// ### Features
+///
+/// - API unification accross platforms.
+/// - Localized menu bar.
 let package = Package(
     name: "SDGInterface",
     products: [
         // The entire package.
+
+        // @documentation(SDGInterface)
+        /// A module representing the entire `SDGInterface` package. All the other modules can be used from this single import.
         .library(name: "SDGInterface", targets: ["SDGInterface"]),
 
         // Individual component modules.
+
+        // @documentation(SDGInterfaceElements)
+        /// Re‐usable interface elements, such as views, windows and menus.
         .library(name: "SDGInterfaceElements", targets: ["SDGInterfaceElements"]),
+
+        // @documentation(SDGApplication)
+        /// Specific, application‐level elements, such as the application delegate and menu bar.
         .library(name: "SDGApplication", targets: ["SDGApplication"])
     ],
     dependencies: [
@@ -31,29 +52,40 @@ let package = Package(
     ],
     targets: [
         // The entire package.
+
+        // #documentation(SDGInterface)
+        /// A module representing the entire `SDGInterface` package. All the other modules can be used from this single import.
         .target(name: "SDGInterface", dependencies: [
             "SDGInterfaceElements",
             "SDGApplication"
             ]),
 
         // Individual component modules.
-        .target(name: "SDGApplication", dependencies: [
-            "SDGInterfaceLocalizations",
-            "SDGInterfaceElements",
-            .productItem(name: "SDGLogic", package: "SDGCornerstone")
-            ]),
+
+        // #documentation(SDGInterfaceElements)
+        /// Re‐usable interface elements, such as views, windows and menus.
         .target(name: "SDGInterfaceElements", dependencies: [
             "SDGInterfaceLocalizations",
             .productItem(name: "SDGControlFlow", package: "SDGCornerstone"),
             .productItem(name: "SDGLocalization", package: "SDGCornerstone")
             ]),
 
+        // #documentation(SDGApplication)
+        /// Specific, application‐level elements, such as the application delegate and menu bar.
+        .target(name: "SDGApplication", dependencies: [
+            "SDGInterfaceLocalizations",
+            "SDGInterfaceElements",
+            .productItem(name: "SDGLogic", package: "SDGCornerstone")
+            ]),
+
         // Internal
+
         .target(name: "SDGInterfaceLocalizations", dependencies: [
             .productItem(name: "SDGLocalization", package: "SDGCornerstone")
             ]),
 
         // Internal tests.
+
         .testTarget(name: "SDGApplicationTests", dependencies: [
             "SDGApplication",
             "SDGInterfaceLocalizations",
