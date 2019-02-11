f527fdfcf66c04251f597201e513af50a17eaa37
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-02-10 18:22:25 -0600

    Refreshed.

--- Sources/SDGInterfaceElements/Screen.swift
+++ Sources/SDGInterfaceElements/Screen.swift
@@ -1,3 +1,16 @@
+/*
+ Screen.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 #if canImport(AppKit)
 // @documentation(Screen)
@@ -5,5 +18,6 @@
 public typealias Screen = NSScreen
 #elseif canImport(UIKit)
 // #documentation(Screen)
+/// An alias for `NSScreen` or `UIScreen`.
 public typealias Screen = UIScreen
 #endif
