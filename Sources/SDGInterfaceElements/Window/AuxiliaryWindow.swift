4770df0700e76fdb824b1b3c7b41f118b65fd265
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-03-21 22:10:15 -0600

    ...

--- Sources/SDGInterfaceElements/Window/AuxiliaryWindow.swift
+++ Sources/SDGInterfaceElements/Window/AuxiliaryWindow.swift
@@ -12,6 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if canImport(AppKit)
 /// An window which plays a supporting role to another window.
 open class AuxiliaryWindow<L>: Window<L> where L : Localization {
 
@@ -31,3 +32,4 @@ open class AuxiliaryWindow<L>: Window<L> where L : Localization {
         collectionBehavior = NSWindow.CollectionBehavior.fullScreenAuxiliary
     }
 }
+#endif
