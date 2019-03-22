8ab1743efbb1e65cea62bf8cde609a772feb6b6f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-03-21 21:23:10 -0600

    Fixed text editor background.

--- Sources/SDGInterfaceElements/View/Text View/TextEditor.swift
+++ Sources/SDGInterfaceElements/View/Text View/TextEditor.swift
@@ -12,10 +12,18 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
+
 import SDGInterfaceLocalizations
 
+#if canImport(AppKit)
+public typealias TextEditorSuperclass = NSScrollView
+#else
+public typealias TextEditorSuperclass = View
+#endif
+
 /// An editor for multiline text.
-public class TextEditor: NSScrollView {
+public class TextEditor : TextEditorSuperclass {
 
     // MARK: - Properties
 
@@ -24,10 +32,24 @@ public class TextEditor: NSScrollView {
     /// Whether or not the background is transparent.
     public var drawsTextBackground: Bool {
         get {
+            #if canImport(AppKit)
             return textView.drawsBackground
+            #else
+            return textView.backgroundColor ≠ nil
+            #endif
         }
         set {
+            #if canImport(AppKit)
             textView.drawsBackground = newValue
+            #else
+            if drawsTextBackground ≠ newValue {
+                if newValue {
+                    textView.backgroundColor = Colour(red: 1, green: 1, blue: 1, alpha: 1)
+                } else {
+                    textView.backgroundColor = nil
+                }
+            }
+            #endif
         }
     }
 
