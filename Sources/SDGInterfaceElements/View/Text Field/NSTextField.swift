26d1e648fe11a32529310899dc72424305d10f1d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-03-22 15:14:56 -0600

    ...

---
+++ Sources/SDGInterfaceElements/View/Text Field/NSTextField.swift
@@ -0,0 +1,17 @@
+/*
+ NSTextField.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if canImport(UIKit)
+public typealias NSTextField = UITextField
+#endif
