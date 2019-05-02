e03ec091d7828af68b920679b55b0a8b2ae9e882
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-01 20:57:55 -0600

    Added documentation.

--- Sources/SDGInterfaceElements/Colour.swift
+++ Sources/SDGInterfaceElements/Colour.swift
@@ -25,6 +25,13 @@ public typealias Colour = UIColor
 extension Colour {
 
     #if !canImport(AppKit)
+    /// Creates a device‐independent colour from red, green and blue components.
+    ///
+    /// - Parameters:
+    ///     - red: The red component. (0–1)
+    ///     - green: The green component. (0–1)
+    ///     - blue: The blue component. (0–1)
+    ///     - alpha: The opacity. (0–1)
     public init(calibratedRed red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
