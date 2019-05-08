ad342f2c51b78e2b687549cdc6cacab2ba1526c0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-08 16:25:32 -0600

    Fixed watchOS build.

--- Sources/SDGInterfaceElements/View/Text Field/TextField.swift
+++ Sources/SDGInterfaceElements/View/Text Field/TextField.swift
@@ -12,6 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if !os(watchOS)
 import SDGInterfaceLocalizations
 
 /// A text field.
@@ -58,3 +59,4 @@ open class TextField : NSTextField {
         return nil
     }
 }
+#endif
