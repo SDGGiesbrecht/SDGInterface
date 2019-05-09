a611b82f6fe605a6c0aa4fe12feb20c996386f34
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-09 15:33:52 -0600

    Fixed watchOS build.

--- Sources/SDGInterfaceElements/View/Table/Table.swift
+++ Sources/SDGInterfaceElements/View/Table/Table.swift
@@ -12,6 +12,8 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if !os(watchOS)
+
 import SDGLogic
 import SDGMathematics
 import SDGLocalization
@@ -319,4 +321,6 @@ extension Table : NSTableViewDelegate {
         return width
     }
 }
+#endif
+
 #endif
