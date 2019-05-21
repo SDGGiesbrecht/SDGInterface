b17786528fb45dbf67fb58e6c522bd1e14a1818f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-21 16:22:42 -0600

    Fixed watchOS build.

--- Sources/SDGInterfaceElements/View/Button/Button.swift
+++ Sources/SDGInterfaceElements/View/Button/Button.swift
@@ -12,6 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if !os(watchOS)
 import SDGInterfaceLocalizations
 
 /// A button.
@@ -72,3 +73,4 @@ public class Button<L>: NSButton, SharedValueObserver where L : Localization {
         #endif
     }
 }
+#endif
--- Sources/SDGInterfaceElements/View/Button/NSButton.swift
+++ Sources/SDGInterfaceElements/View/Button/NSButton.swift
@@ -12,7 +12,11 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if !os(watchOS)
+
 #if !canImport(AppKit)
 /// An `AppKit.NSButton` or a `UIButton`.
 public typealias NSButton = UIButton
+#endif
+
 #endif
