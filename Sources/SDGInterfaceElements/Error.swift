7233f255ea5fa085b4db4a4378651b97078bb85e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-26 20:48:14 -0600

    Fixed iOS.

--- Sources/SDGInterfaceElements/Error.swift
+++ Sources/SDGInterfaceElements/Error.swift
@@ -3,16 +3,32 @@ import Dispatch
 
 import SDGLocalization
 
+import SDGInterfaceLocalizations
+
 extension Error {
 
     // MARK: - Display
 
     /// Displays the error to the user.
     public func display() { // @exempt(from: tests) Requires user interaction.
+        #if canImport(AppKit)
         let cocoaError = self as NSError
         var info = cocoaError.userInfo
         info[NSLocalizedDescriptionKey] = localizedDescription
         let modified = NSError(domain: cocoaError.domain, code: cocoaError.code, userInfo: info)
         NSAlert(error: modified).runModal()
+        #else
+        let alert = UIAlertController(title: localizedDescription, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(
+            title: String(UserFacing<StrictString, InterfaceLocalization>({ localization in
+                switch localization {
+                case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                    return "Okay"
+                }
+            }).resolved()),
+            style: .cancel,
+            handler: nil))
+        UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
+        #endif
     }
 }
