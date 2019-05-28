22990cd223840430bef1bd79556782123ebd766c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-28 16:25:04 -0600

    Added window types.

---
+++ Sources/SDGInterfaceElements/Window/FullscreenWindow.swift
@@ -0,0 +1,23 @@
+/*
+ FullscreenWindow.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+/// A window that begins in fullscreen mode.
+public class FullscreenWindow<L>: PrimaryWindow<L> where L : Localization {
+
+    /// Creates a fullscreen window.
+    public override init(title: Shared<UserFacing<StrictString, L>>) {
+        super.init(title: title)
+        self.isFullscreen = true
+    }
+}
---
+++ Sources/SDGInterfaceElements/Window/PrimaryWindow.swift
@@ -0,0 +1,26 @@
+/*
+ PrimaryWindow.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+/// A window that can serve as the primary window.
+public class PrimaryWindow<L>: Window<L> where L : Localization {
+
+    /// Creates a window.
+    ///
+    /// - Parameters:
+    ///     - title: The title.
+    public init(title: Shared<UserFacing<StrictString, L>>) {
+        super.init(title: title, size: Screen.mainScreen.frame.size)
+        collectionBehavior = .fullScreenPrimary
+    }
+}
