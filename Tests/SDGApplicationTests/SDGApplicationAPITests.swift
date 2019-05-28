58315db660c03d2df8a5aabef3e9b910dd32426c
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-28 16:29:34 -0600

    Fixed macOS tests.

--- Tests/SDGApplicationTests/SDGApplicationAPITests.swift
+++ Tests/SDGApplicationTests/SDGApplicationAPITests.swift
@@ -640,8 +640,7 @@ final class SDGApplicationAPITests : ApplicationTestCase {
     }
 
     func testWindow() {
-        SampleApplicationDelegate().demonstrateWindow()
-
+        SampleApplicationDelegate().demonstrateFullscreenWindow()
 
         let window = Window(title: Shared(UserFacing<StrictString, InterfaceLocalization>({ _ in "Title" })), size: CGSize(width: 700, height: 300))
         #if canImport(AppKit) // UIKit raises an exception during tests.
