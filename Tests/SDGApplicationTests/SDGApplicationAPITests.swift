cc571a8b83a77cc52f3da123bed39c46f734487e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-02 20:12:02 -0600

    Added keyboard information.

--- Tests/SDGApplicationTests/SDGApplicationAPITests.swift
+++ Tests/SDGApplicationTests/SDGApplicationAPITests.swift
@@ -218,6 +218,21 @@ final class SDGApplicationAPITests : ApplicationTestCase {
         SampleApplicationDelegate().demonstrateImage()
     }
 
+    func testKey() {
+        XCTAssert(Key.rightIndexHome.hasConsistentPosition)
+        #if canImport(Carbon)
+        let name = String(Key.rightIndexHome.currentName)
+        XCTAssertEqual(name, name.uppercased())
+        XCTAssertFalse(name.isEmpty)
+        XCTAssertEqual(Key(code: Key.rightIndexHome.keyCode), Key.rightIndexHome)
+        XCTAssertNil(Key(code: CGKeyCode.max))
+        #endif
+        XCTAssertFalse(Key.かなジス.hasConsistentPosition)
+        XCTAssert(Key.rightIndexHome.existsConsistently)
+        XCTAssertFalse(Key.かなジス.existsConsistently)
+        XCTAssert(Key.rightDoubleOutsideHomeISO_JIS_RightTripleOutsideUpperANSI.existsConsistently)
+    }
+
     func testLabel() {
         SampleApplicationDelegate().demonstrateLabel()
         forEachWindow { window in
