fcac1a4f29c22b6482a26d5dd3c53761c51cc8a1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-04 21:20:30 -0600

    Fixed watchOS.

--- Sources/SDGInterfaceElements/View/Text Field/LabelledTextField.swift
+++ Sources/SDGInterfaceElements/View/Text Field/LabelledTextField.swift
@@ -12,6 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if !os(watchOS)
 import SDGInterfaceLocalizations
 
 /// A text field with a label.
@@ -65,3 +66,4 @@ open class LabelledTextField<L> : View where L : Localization {
     /// The field.
     public let field: TextField
 }
+#endif
