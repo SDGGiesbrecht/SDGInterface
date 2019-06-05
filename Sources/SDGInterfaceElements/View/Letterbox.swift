408b453648d3db4f35ee74f09a5d6c4c5647230a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-05 17:32:38 -0600

    Fixed watchOS.

--- Sources/SDGInterfaceElements/View/Letterbox.swift
+++ Sources/SDGInterfaceElements/View/Letterbox.swift
@@ -12,6 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+#if !os(watchOS)
 import SDGMathematics
 
 import SDGInterfaceLocalizations
@@ -92,11 +93,20 @@ open class Letterbox: View {
         if let colour = self.colour {
             colour.setFill()
             if colour.alphaComponent ≤ 1 {
+                #if canImport(UIKit)
+                UIRectFillUsingBlendMode(dirtyRect, .normal)
+                #else
                 dirtyRect.fill(using: .sourceOver)
+                #endif
             } else {
+                #if canImport(UIKit)
+                UIRectFill(dirtyRect)
+                #else
                 dirtyRect.fill()
+                #endif
             }
         }
         super.draw(dirtyRect)
     }
 }
+#endif
