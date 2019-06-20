20d5b9fe767ed2522df47ce943ad722c2a9bddbf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-19 19:04:43 -0600

    ...

--- Sources/SDGMenuBar/MenuBarFormatFontKern.swift
+++ Sources/SDGMenuBar/MenuBarFormatFontKern.swift
@@ -25,8 +25,92 @@ import SDGInterfaceLocalizations
 
 extension MenuBar {
 
+    private static func useDefault() -> MenuEntry<MenuBarLocalization> {
+        let useDefault = MenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
+            switch localization {
+            case .españolEspaña:
+                return "Valor por omisión"
+            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                return "Use Default"
+            case .deutschDeutschland:
+                return "Normal"
+            case .françaisFrance:
+                return "Valeur par défaut"
+            case .ελληνικάΕλλάδα:
+                return "Χρήση προεπιλογής"
+            case .עברית־ישראל:
+                return "השתמש בברירת המחדל"
+            }
+        })))
+        useDefault.action = #selector(NSTextView.useStandardKerning(_:))
+        return useDefault
+    }
+
+    private static func useNone() -> MenuEntry<MenuBarLocalization> {
+        let useNone = MenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
+            switch localization {
+            case .españolEspaña:
+                return "Ninguno"
+            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                return "Use None"
+            case .deutschDeutschland:
+                return "Nicht verwenden"
+            case .françaisFrance:
+                return "Aucun"
+            case .ελληνικάΕλλάδα:
+                return "Καμία"
+            case .עברית־ישראל:
+                return "אל תשתמש בשום אפשרות"
+            }
+        })))
+        useNone.action = #selector(NSTextView.turnOffKerning(_:))
+        return useNone
+    }
+
+    private static func tighten() -> MenuEntry<MenuBarLocalization> {
+        let tighten = MenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
+            switch localization {
+            case .españolEspaña:
+                return "Reducir"
+            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                return "Tighten"
+            case .deutschDeutschland:
+                return "Enger"
+            case .françaisFrance:
+                return "Resserrer"
+            case .ελληνικάΕλλάδα:
+                return "Πιο κοντά"
+            case .עברית־ישראל:
+                return "הדוק יותר"
+            }
+        })))
+        tighten.action = #selector(NSTextView.tightenKerning(_:))
+        return tighten
+    }
+
+    private static func loosen() -> MenuEntry<MenuBarLocalization> {
+        let loosen = MenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
+            switch localization {
+            case .españolEspaña:
+                return "Aumentar"
+            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
+                return "Loosen"
+            case .deutschDeutschland:
+                return "Weiter"
+            case .françaisFrance:
+                return "Desserrer"
+            case .ελληνικάΕλλάδα:
+                return "Πιο αραιά"
+            case .עברית־ישראל:
+                return "מרווח יותר"
+            }
+        })))
+        loosen.action = #selector(NSTextView.loosenKerning(_:))
+        return loosen
+    }
+
     internal static func kern() -> Menu<MenuBarLocalization> {
-        let kern = font.Menu(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
+        let kern = Menu(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
             switch localization {
             case .españolEspaña:
                 return "Interletraje"
@@ -44,73 +128,12 @@ extension MenuBar {
                 return "מרווח בין אותיות"
             }
         })))
-
-        kernMenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
-            switch localization {
-            case .españolEspaña:
-                return "Valor por omisión"
-            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
-                return "Use Default"
-            case .deutschDeutschland:
-                return "Normal"
-            case .françaisFrance:
-                return "Valeur par défaut"
-            case .ελληνικάΕλλάδα:
-                return "Χρήση προεπιλογής"
-            case .עברית־ישראל:
-                return "השתמש בברירת המחדל"
-            }
-        })), action: #selector(NSTextView.useStandardKerning(_:)))
-
-        kernMenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
-            switch localization {
-            case .españolEspaña:
-                return "Ninguno"
-            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
-                return "Use None"
-            case .deutschDeutschland:
-                return "Nicht verwenden"
-            case .françaisFrance:
-                return "Aucun"
-            case .ελληνικάΕλλάδα:
-                return "Καμία"
-            case .עברית־ישראל:
-                return "אל תשתמש בשום אפשרות"
-            }
-        })), action: #selector(NSTextView.turnOffKerning(_:)))
-
-        kernMenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
-            switch localization {
-            case .españolEspaña:
-                return "Reducir"
-            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
-                return "Tighten"
-            case .deutschDeutschland:
-                return "Enger"
-            case .françaisFrance:
-                return "Resserrer"
-            case .ελληνικάΕλλάδα:
-                return "Πιο κοντά"
-            case .עברית־ישראל:
-                return "הדוק יותר"
-            }
-        })), action: #selector(NSTextView.tightenKerning(_:)))
-
-        kernMenuEntry(label: .static(UserFacing<StrictString, MenuBarLocalization>({ localization in
-            switch localization {
-            case .españolEspaña:
-                return "Aumentar"
-            case .englishUnitedKingdom, .englishUnitedStates, .englishCanada:
-                return "Loosen"
-            case .deutschDeutschland:
-                return "Weiter"
-            case .françaisFrance:
-                return "Desserrer"
-            case .ελληνικάΕλλάδα:
-                return "Πιο αραιά"
-            case .עברית־ישראל:
-                return "מרווח יותר"
-            }
-        })), action: #selector(NSTextView.loosenKerning(_:)))
+        kern.entries = [
+            .entry(useDefault()),
+            .entry(useNone()),
+            .entry(tighten()),
+            .entry(loosen())
+        ]
+        return kern
     }
 }
