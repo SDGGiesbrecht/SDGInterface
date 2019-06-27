793e8a2c6251f286ab207032137f887a0f8e21bf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-26 20:00:21 -0600

    Version 0.1.0

--- Workspace.swift
+++ Workspace.swift
@@ -17,7 +17,7 @@ import WorkspaceConfiguration
 let configuration = WorkspaceConfiguration()
 configuration._applySDGDefaults()
 
-configuration.documentation.currentVersion = Version(0, 0, 0)
+configuration.documentation.currentVersion = Version(0, 1, 0)
 
 configuration.documentation.projectWebsite = URL(string: "https://sdggiesbrecht.github.io/SDGInterface")!
 configuration.documentation.documentationURL = URL(string: "https://sdggiesbrecht.github.io/SDGInterface")!
