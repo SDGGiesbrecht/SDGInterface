f1d6eab735dbf6763001046ca70de7efba8a3574
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-06-28 19:48:58 -0600

    More test refactors.

--- Tests/SDGMenusTests/APITests.swift
+++ Tests/SDGMenusTests/APITests.swift
@@ -45,4 +45,71 @@ final class APITests : ApplicationTestCase {
         XCTAssertEqual(menu.native.title, title)
         #endif
     }
+
+    func testMenuComponent() {
+        XCTAssertNotNil(MenuComponent.entry(MenuEntry<InterfaceLocalization>(label: .binding(Shared("")))).asEntry)
+        XCTAssertNotNil(MenuComponent.submenu(Menu<InterfaceLocalization>(label: .binding(Shared("")))).asSubmenu)
+        XCTAssertNil(MenuComponent.submenu(Menu<InterfaceLocalization>(label: .binding(Shared("")))).asEntry)
+        XCTAssertNil(MenuComponent.entry(MenuEntry<InterfaceLocalization>(label: .binding(Shared("")))).asSubmenu)
+    }
+
+    func testMenuEntry() {
+        let menuLabel = Shared<StrictString>("initial")
+        let menu = MenuEntry<APILocalization>(label: .binding(menuLabel))
+        menuLabel.value = "changed"
+        XCTAssertEqual(menu.label.resolved(), menuLabel.value)
+        let separateMenuLabel = Shared<StrictString>("separate")
+        menu.label = .binding(separateMenuLabel)
+        XCTAssertEqual(menu.label.resolved(), separateMenuLabel.value)
+        menuLabel.value = "unrelated"
+        XCTAssertEqual(menu.label.resolved(), separateMenuLabel.value)
+        let action = #selector(NSObject.isEqual(_:))
+        menu.action = action
+        _ = menu.action
+        #if !os(watchOS) && !os(tvOS)
+        XCTAssertEqual(menu.action, action)
+        #endif
+        menu.action = nil
+        let target = NSObject()
+        menu.target = target
+        _ = menu.target
+        #if canImport(AppKit)
+        XCTAssertEqual(menu.target as? NSObject, target)
+        #endif
+        let hotKey = "A"
+        menu.hotKey = hotKey
+        _ = menu.hotKey
+        #if canImport(AppKit)
+        XCTAssertEqual(menu.hotKey, hotKey)
+        #endif
+        let modifiers: KeyModifiers = .command
+        menu.hotKeyModifiers = modifiers
+        _ = menu.hotKeyModifiers
+        #if canImport(AppKit)
+        XCTAssertEqual(menu.hotKeyModifiers, modifiers)
+        #endif
+        menu.isHidden = true
+        _ = menu.isHidden
+        #if canImport(AppKit)
+        XCTAssert(menu.isHidden)
+        #endif
+        menu.indentationLevel = 1
+        _ = menu.indentationLevel
+        #if canImport(AppKit)
+        XCTAssertEqual(menu.indentationLevel, 1)
+        #endif
+        menu.tag = 1
+        XCTAssertEqual(menu.tag, 1)
+        #if !os(watchOS) && !os(tvOS)
+        let title = menu.native.title
+        #endif
+        #if canImport(AppKit)
+        menu.native = NSMenuItem()
+        #elseif canImport(UIKit) && !os(watchOS) && !os(tvOS)
+        menu.native = UIMenuItem()
+        #endif
+        #if !os(watchOS) && !os(tvOS)
+        XCTAssertEqual(menu.native.title, title)
+        #endif
+    }
 }
