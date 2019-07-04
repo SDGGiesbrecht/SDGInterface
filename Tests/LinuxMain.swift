f899df3da93779216e7f42bb7cdd8b0e26da3a84
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-07-03 20:51:32 -0600

    Refreshed test manifesta

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,6 +3,7 @@ import XCTest
 import SDGApplicationTests
 import SDGContextMenuTests
 import SDGInterfaceBasicsTests
+import SDGKeyboardTests
 import SDGMenuBarTests
 import SDGMenusTests
 import SDGWindowsTests
@@ -11,6 +12,7 @@ var tests = [XCTestCaseEntry]()
 tests += SDGApplicationTests.__allTests()
 tests += SDGContextMenuTests.__allTests()
 tests += SDGInterfaceBasicsTests.__allTests()
+tests += SDGKeyboardTests.__allTests()
 tests += SDGMenuBarTests.__allTests()
 tests += SDGMenusTests.__allTests()
 tests += SDGWindowsTests.__allTests()
