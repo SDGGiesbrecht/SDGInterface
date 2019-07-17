f79c8f3d3335f8bb2a703d3a11e6d7c0a971a5c2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-07-17 14:20:59 -0600

    Encapsulated check box.

--- Sources/SDGButtons/CheckBox.swift
+++ Sources/SDGButtons/CheckBox.swift
@@ -1,5 +1,5 @@
 /*
- Button.swift
+ CheckBox.swift
 
  This source file is part of the SDGInterface open source project.
  https://sdggiesbrecht.github.io/SDGInterface
@@ -12,13 +12,8 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
 #if canImport(AppKit)
 import AppKit
-#endif
-#if canImport(UIKit)
-import UIKit
-#endif
 
 import SDGText
 import SDGLocalization
@@ -26,8 +21,8 @@ import SDGLocalization
 import SDGInterfaceBasics
 import SDGViews
 
-/// A button.
-public final class Button<L> : AnyButton, SpecificView where L : Localization {
+/// A check box.
+public final class CheckBox<L> : AnyCheckBox, SpecificView where L : Localization {
 
     // MARK: - Initialization
 
@@ -42,30 +37,22 @@ public final class Button<L> : AnyButton, SpecificView where L : Localization {
             LocalizationSetting.current.register(observer: bindingObserver)
         }
 
-        #if canImport(AppKit)
         specificNative = NSButton()
-        #elseif canImport(UIKit)
-        specificNative = UIButton()
-        #endif
         defer {
-            bindingObserver.button = self
+            bindingObserver.checkBox = self
         }
 
-        #if canImport(AppKit)
         specificNative.bezelStyle = .rounded
-        specificNative.setButtonType(.momentaryPushIn)
-        #endif
+        specificNative.setButtonType(.switch)
 
-        #if canImport(AppKit)
+        specificNative.lineBreakMode = .byTruncatingTail
+
         specificNative.font = Font.forLabels.native
-        #elseif canImport(UIKit)
-        specificNative.titleLabel?.font = Font.forLabels.native
-        #endif
     }
 
     // MARK: - Properties
 
-    private let bindingObserver = ButtonBindingObserver()
+    private let bindingObserver = CheckBoxBindingObserver()
 
     /// The label.
     public var label: Binding<StrictString, L> {
@@ -84,19 +71,11 @@ public final class Button<L> : AnyButton, SpecificView where L : Localization {
 
     public func _refreshBindings() {
         let resolved = String(label.resolved())
-        #if canImport(AppKit)
         specificNative.title = resolved
-        #elseif canImport(UIKit)
-        specificNative.titleLabel?.text = resolved
-        #endif
     }
 
     // MARK: - SpecificView
 
-    #if canImport(AppKit)
     public let specificNative: NSButton
-    #elseif canImport(UIKit)
-    public let specificNative: UIButton
-    #endif
 }
 #endif
---
+++ Sources/SDGButtons/CheckBoxBindingObserver.swift
@@ -0,0 +1,30 @@
+/*
+ CheckBoxBindingObserver.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
+import SDGControlFlow
+
+internal final class CheckBoxBindingObserver : SharedValueObserver {
+
+    // MARK: - Properties
+
+    internal weak var checkBox: AnyCheckBox?
+
+    // MARK: - SharedValueObserver
+
+    internal func valueChanged(for identifier: String) {
+        checkBox?.refreshBindings()
+    }
+}
+#endif
