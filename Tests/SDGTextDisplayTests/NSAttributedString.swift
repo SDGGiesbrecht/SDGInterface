589aba358a403b94cb3a196921c05bbafaccf5f9
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-27 01:04:56 -0600

    Fixed iOS test.

--- Tests/SDGTextDisplayTests/NSAttributedString.swift
+++ Tests/SDGTextDisplayTests/NSAttributedString.swift
@@ -30,3 +30,37 @@ extension NSAttributedString {
     }
     #endif
 }
+
+ #if canImport(UIKit)
+extension SemanticMarkup {
+
+    // #workaround(SDGCornerstone 2.5.1, The upstream version does not handle new iOS text mechanism yet.)
+    fileprivate static func _attributedString(from html: String, in font: Font) throws -> NSAttributedString {
+        var adjustedFontName = font.fontName
+
+        #if canImport(UIKit)
+        if #available(iOS 13, watchOS 6, tvOS 13, *) {
+            // Older platforms do not support this CSS, but can use the name directly.
+            if adjustedFontName == Font.system.fontName {
+                adjustedFontName = "-apple-system"
+            }
+        }
+        #endif
+
+        var modified = "<span style=\u{22}"
+
+        modified += "font\u{2D}family: &#x22;" + adjustedFontName + "&#x22;;"
+        modified += "font\u{2D}size: \(font.size)pt;"
+
+        modified += "\u{22}>"
+        modified += html
+        modified += "</span>"
+
+        let data = modified.data(using: .utf8)!
+        return try NSAttributedString(data: data, options: [
+            .characterEncoding: NSNumber(value: String.Encoding.utf8.rawValue),
+            .documentType: NSAttributedString.DocumentType.html
+            ], documentAttributes: nil)
+    }
+}
+#endif
