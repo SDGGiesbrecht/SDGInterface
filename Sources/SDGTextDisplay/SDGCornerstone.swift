b6d4fe7e85b91a149e6578be86b234db6bbba5b7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-27 19:13:26 -0600

    Fixed style.

--- Sources/SDGTextDisplay/SDGCornerstone.swift
+++ Sources/SDGTextDisplay/SDGCornerstone.swift
@@ -35,7 +35,7 @@ extension SemanticMarkup {
         if #available(iOS 13, watchOS 6, tvOS 13, *) { // @exempt(from: unicode)
             // Older platforms do not support this CSS, but can use the name directly.
             if adjustedFontName == Font.system.fontName {
-                adjustedFontName = "-apple-system"
+                adjustedFontName = "\u{2D}apple\u{2D}system"
             }
         }
 
