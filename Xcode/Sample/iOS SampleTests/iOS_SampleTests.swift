85c250cff81169eaf71c1de2ec7b0ec400bc391d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-10-10 21:36:45 -0600

    Refreshed.

--- Xcode/Sample/iOS SampleTests/iOS_SampleTests.swift
+++ Xcode/Sample/iOS SampleTests/iOS_SampleTests.swift
@@ -1,9 +1,16 @@
-//
-//  iOS_SampleTests.swift
-//  iOS SampleTests
-//
-//  Created by Jeremy on 2019‐10‐10.
-//
+/*
+ iOS_SampleTests.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 import XCTest
 @testable import iOS_Sample
