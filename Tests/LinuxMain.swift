af010b15abaa6d85fb8c19ec7bb59370ff8caaa7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-03 15:50:11 -0600

    Removed test manifests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -11,38 +11,3 @@
  Licensed under the Apache Licence, Version 2.0.
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
-
-import XCTest
-
-import SDGApplicationTests
-import SDGButtonsTests
-import SDGContextMenuTests
-import SDGImageDisplayTests
-import SDGInterfaceBasicsTests
-import SDGKeyboardTests
-import SDGMenuBarTests
-import SDGMenusTests
-import SDGPopOversTests
-import SDGProgressIndicatorsTests
-import SDGTablesTests
-import SDGTextDisplayTests
-import SDGViewsTests
-import SDGWindowsTests
-
-var tests = [XCTestCaseEntry]()
-tests += SDGApplicationTests.__allTests()
-tests += SDGButtonsTests.__allTests()
-tests += SDGContextMenuTests.__allTests()
-tests += SDGImageDisplayTests.__allTests()
-tests += SDGInterfaceBasicsTests.__allTests()
-tests += SDGKeyboardTests.__allTests()
-tests += SDGMenuBarTests.__allTests()
-tests += SDGMenusTests.__allTests()
-tests += SDGPopOversTests.__allTests()
-tests += SDGProgressIndicatorsTests.__allTests()
-tests += SDGTablesTests.__allTests()
-tests += SDGTextDisplayTests.__allTests()
-tests += SDGViewsTests.__allTests()
-tests += SDGWindowsTests.__allTests()
-
-XCTMain(tests)
