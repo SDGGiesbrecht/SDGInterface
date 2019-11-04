c358e5e5e3bd48732f7d88159b1525710471ef20
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-03 18:07:26 -0600

    Updated Workspace.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,34 +1,13 @@
-import XCTest
+/*
+ LinuxMain.swift
 
-import SDGApplicationTests
-import SDGButtonsTests
-import SDGContextMenuTests
-import SDGImageDisplayTests
-import SDGInterfaceBasicsTests
-import SDGKeyboardTests
-import SDGMenuBarTests
-import SDGMenusTests
-import SDGPopOversTests
-import SDGProgressIndicatorsTests
-import SDGTablesTests
-import SDGTextDisplayTests
-import SDGViewsTests
-import SDGWindowsTests
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
 
-var tests = [XCTestCaseEntry]()
-tests += SDGApplicationTests.__allTests()
-tests += SDGButtonsTests.__allTests()
-tests += SDGContextMenuTests.__allTests()
-tests += SDGImageDisplayTests.__allTests()
-tests += SDGInterfaceBasicsTests.__allTests()
-tests += SDGKeyboardTests.__allTests()
-tests += SDGMenuBarTests.__allTests()
-tests += SDGMenusTests.__allTests()
-tests += SDGPopOversTests.__allTests()
-tests += SDGProgressIndicatorsTests.__allTests()
-tests += SDGTablesTests.__allTests()
-tests += SDGTextDisplayTests.__allTests()
-tests += SDGViewsTests.__allTests()
-tests += SDGWindowsTests.__allTests()
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
 
-XCTMain(tests)
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
