14673f54b4e1be964cdef6b014f41ed5b8b03be1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-04 22:24:35 -0600

    Enabled View protocol on watchOS.

--- Sources/SDGViews/View.swift
+++ Sources/SDGViews/View.swift
@@ -12,7 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
+#if canImport(SwiftUI) || canImport(AppKit) || canImport(UIKit)
   #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
     import SwiftUI
   #endif
@@ -24,26 +24,27 @@
 
   /// A view.
   public protocol View: AnyObject {
+
+    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+      /// The SwiftUI view.
+      @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+      var swiftUIView: SwiftUI.AnyView { get }
+    #endif
+
     #if canImport(AppKit)
       // @documentation(View.cocoaView)
       /// The `NSView` or `UIView`.
       var cocoaView: NSView { get }
-    #elseif canImport(UIKit)
+    #elseif canImport(UIKit) && !os(watchOS)
       // #documentation(View.cocoaView)
       /// The `NSView` or `UIView`.
       var cocoaView: UIView { get }
-    #endif
-
-    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
-      /// The SwiftUI view.
-      @available(macOS 10.15, iOS 13, tvOS 13, *)
-      var swiftUIView: SwiftUI.AnyView { get }
     #endif
   }
 
   extension View {
 
-    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+    #if canImport(SwiftUI) && !os(watchOS) && !(os(iOS) && arch(arm))
       @available(macOS 10.15, iOS 13, tvOS 13, *)
       public var swiftUIView: AnyView {
         // @exempt(from: tests) #workaround(workspace version 0.27.0, macOS 10.15 is unavailable in CI.)
@@ -53,21 +54,24 @@
 
     // MARK: - Aspect Ratio
 
-    /// Locks the aspect ratio of the view.
-    ///
-    /// - Parameters:
-    ///     - aspectRatio: The aspect ratio. (*width* ∶ *height*)
-    public func lockAspectRatio(to aspectRatio: Double) {
-      let constraint = NSLayoutConstraint(
-        item: self.cocoaView,
-        attribute: .width,
-        relatedBy: .equal,
-        toItem: self.cocoaView,
-        attribute: .height,
-        multiplier: CGFloat(aspectRatio),
-        constant: 0
-      )
-      cocoaView.addConstraint(constraint)
-    }
+    #if !os(watchOS)
+      // #workaround(Can this be done with SwiftUI?)
+      /// Locks the aspect ratio of the view.
+      ///
+      /// - Parameters:
+      ///     - aspectRatio: The aspect ratio. (*width* ∶ *height*)
+      public func lockAspectRatio(to aspectRatio: Double) {
+        let constraint = NSLayoutConstraint(
+          item: self.cocoaView,
+          attribute: .width,
+          relatedBy: .equal,
+          toItem: self.cocoaView,
+          attribute: .height,
+          multiplier: CGFloat(aspectRatio),
+          constant: 0
+        )
+        cocoaView.addConstraint(constraint)
+      }
+    #endif
   }
 #endif
