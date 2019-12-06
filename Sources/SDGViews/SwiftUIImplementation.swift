5c7ce8e796bea88ef36bde251dcbe8fc7fb1752d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-06 14:21:55 -0600

    Added direct implementation.

---
+++ Sources/SDGViews/SwiftUIImplementation.swift
@@ -0,0 +1,39 @@
+/*
+ SwiftUIImplementation.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if canImport(SwiftUI)
+  import SwiftUI
+
+  @available(macOS 10.15, *)
+  public protocol SwiftUIImplementation: View, SwiftUI.View {}
+
+  @available(macOS 10.15, *)
+  extension SwiftUIImplementation {
+
+    public var swiftUIView: AnyView {
+      return AnyView(self)
+    }
+
+    #if canImport(AppKit)
+      public var cocoaView: NSView {
+        return NSHostingView(rootView: self)
+      }
+    #elseif canImport(UIKit) && !os(watchOS)
+      public var cocoaView: UIView {
+        let controller = UIHostingController(rootView: self)
+        specificCocoaView = controller.view
+      }
+    #endif
+  }
+#endif
---
+++ Tests/SDGViewsTests/SwiftUIExample.swift
@@ -0,0 +1,29 @@
+/*
+ SwiftUIExample.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if canImport(SwiftUI)
+  import SwiftUI
+
+  import SDGViews
+
+  @available(macOS 10.15, *)
+  struct SwiftUIExample: SwiftUIImplementation {
+
+    // MARK: - View
+
+    var body: some SwiftUI.View {
+      return Text(verbatim: "...")
+    }
+  }
+#endif
