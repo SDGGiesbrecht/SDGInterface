878c80db9e1e38a69e1b9693faa8df4acbd53d4d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-08 16:45:43 -0600

    Renamed file.

--- Sources/SDGViews/CocoaViewImplementation.swift
+++ Sources/SDGViews/CocoaViewImplementation.swift
@@ -1,5 +1,5 @@
 /*
- CocoaImplementation.swift
+ CocoaViewImplementation.swift
 
  This source file is part of the SDGInterface open source project.
  https://sdggiesbrecht.github.io/SDGInterface
--- Sources/SDGViews/SwiftUIViewImplementation.swift
+++ Sources/SDGViews/SwiftUIViewImplementation.swift
@@ -1,5 +1,5 @@
 /*
- SwiftUIImplementation.swift
+ SwiftUIViewImplementation.swift
 
  This source file is part of the SDGInterface open source project.
  https://sdggiesbrecht.github.io/SDGInterface
