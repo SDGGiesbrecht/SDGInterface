51eddcbe2be0c6bebecc1d40493801f453eb10b9
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-11 17:34:33 -0600

    Refactored preview.

--- Xcode/Sample/SwiftUI Previews/AspectRatio.swift
+++ Xcode/Sample/SwiftUI Previews/AspectRatio.swift
@@ -9,33 +9,30 @@ struct AspectRatio_Previews: PreviewProvider {
   static var previews: some SwiftUI.View {
 
     let redSquare = Ellipse()
-      .fill(Color.red)
+      .fill(Color.black)
 
+    func preview<V>(_ view: V, name: String) -> some SwiftUI.View where V: SwiftUI.View {
+      return view
+        .previewLayout(.fixed(width: 124, height: 64))
+        .previewDisplayName(name)
+    }
+    
     return Group {
-
-      redSquare
-        .aspectRatio(1 ÷ 2, contentMode: .fill)
-        .background(Color.blue)
-        .previewLayout(.fixed(width: 124, height: 64))
-        .previewDisplayName("Ratio + Fill")
       
-      redSquare
-        .aspectRatio(1 ÷ 2, contentMode: .fit)
-        .background(Color.blue)
-        .previewLayout(.fixed(width: 124, height: 64))
-        .previewDisplayName("Ratio + Fit")
-
-        redSquare
-          .aspectRatio(nil, contentMode: .fill)
-          .background(Color.blue)
-          .previewLayout(.fixed(width: 124, height: 64))
-          .previewDisplayName("nil + Fill")
-
-        redSquare
-          .aspectRatio(nil, contentMode: .fit)
-          .background(Color.blue)
-          .previewLayout(.fixed(width: 124, height: 64))
-          .previewDisplayName("nil + Fit")
+      preview(redSquare
+        .aspectRatio(nil, contentMode: .fill), name: "nil + .fill")
+      
+      preview(redSquare
+        .aspectRatio(nil, contentMode: .fit),
+              name: "nil + .fit")
+      
+      preview(redSquare
+        .aspectRatio(1 ÷ 2, contentMode: .fill),
+              name: "(1 ÷ 2) + .fill")
+      
+      preview(redSquare
+        .aspectRatio(1 ÷ 2, contentMode: .fit),
+              name: "(1 ÷ 2) + .fit")
     }
   }
 }
