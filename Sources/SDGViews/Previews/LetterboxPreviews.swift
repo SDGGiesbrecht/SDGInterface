3a71136954f3b3511e023dc410a71fef35d189c8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-06 23:03:48 -0600

    Determined equivalents.

--- Sources/SDGViews/Previews/LetterboxPreviews.swift
+++ Sources/SDGViews/Previews/LetterboxPreviews.swift
@@ -19,19 +19,43 @@
   @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
   internal struct LetterboxPreviews: SwiftUI.View {
 
-    internal var body: some SwiftUI.View {
-      func makeView() -> Letterbox<Ellipse> {
-        return Letterbox(content: Ellipse(), aspectRatio: 1)
+    /// Previews in one mode.
+    func preview(width: CGFloat, height: CGFloat, legacyMode: Bool) -> some SwiftUI.View {
+      let anyView: AnyView
+      if legacyMode {
+        let letterbox = Letterbox(content: Ellipse(), aspectRatio: 1)
+        letterbox.colour = .red
+        anyView = AnyView(letterbox.swiftUIView)
+      } else {
+        let letterbox = Ellipse()
+          .aspectRatio(1 as Double, contentMode: .fit)
+          .swiftUIView
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+          .background(Color.red)
+        anyView = AnyView(letterbox)
       }
+      return
+        anyView
+        .frame(width: width, height: height)
+        .padding(1)
+        .border(Color.gray, width: 1)
+    }
+
+    /// Previews in both modes side‐by‐side.
+    func preview(width: CGFloat, height: CGFloat, name: String) -> some SwiftUI.View {
+      let stack = HStack(spacing: 8) {
+        preview(width: width, height: height, legacyMode: false)
+        preview(width: width, height: height, legacyMode: true)
+      }
+      return
+        stack
+        .previewDisplayName(name)
+    }
+
+    internal var body: some SwiftUI.View {
       return Group {
-
-        makeView().swiftUIView
-          .previewLayout(.fixed(width: 128, height: 64))
-          .previewDisplayName("Pillarbox")
-
-        makeView().swiftUIView
-          .previewLayout(.fixed(width: 64, height: 128))
-          .previewDisplayName("Letterbox")
+        preview(width: 128, height: 64, name: "Pillarbox")
+        preview(width: 64, height: 128, name: "Letterbox")
       }
     }
   }
