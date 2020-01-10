b44bfc16e2ae7f891a5203f38a8e3659482d1707
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-09 21:10:06 -0600

    Added colour container type.

---
+++ Sources/SDGViews/Cocoa/ColourContainer.swift
@@ -0,0 +1,45 @@
+/*
+ ColourContainer.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019–2020 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if canImport(AppKit)
+  import AppKit
+
+  import SDGInterfaceBasics
+
+  internal class ColourContainer: NSView, CocoaViewImplementation {
+
+    // MARK: - Initialization
+
+    internal init(_ colour: Colour) {
+      self.colour = colour
+      super.init(frame: NSRect.zero)
+    }
+
+    required init?(coder: NSCoder) {  // @exempt(from: tests)
+      return nil
+    }
+
+    // MARK: - Properties
+
+    private let colour: Colour
+
+    // MARK: - NSView
+
+    internal override func draw(_ dirtyRect: CGRect) {
+      colour.nsColor.setFill()
+      dirtyRect.fill(using: .sourceOver)
+      super.draw(dirtyRect)
+    }
+  }
+#endif
--- Sources/SDGViews/Colour.swift
+++ Sources/SDGViews/Colour.swift
@@ -13,33 +13,37 @@
  */
 
 #if canImport(SwiftUI) || canImport(AppKit) || canImport(UIKit)
-#if canImport(SwiftUI) && !(os(iOS) && arch(arm))
-  import SwiftUI
-#endif
-#if canImport(AppKit)
-  import AppKit
-#elseif canImport(UIKit)
-  import UIKit
-#endif
-
-import SDGInterfaceBasics
-
-extension Colour: View {
-
   #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
-    @available(macOS 10.15, iOS 13, tvOS 13, *)
-    public var swiftUIView: AnyView {
-      return AnyView(SwiftUI.Color(self))
-    }
+    import SwiftUI
   #endif
-
   #if canImport(AppKit)
-      public var cocoaView: NSView {
-
-      }
-  #else
-      public var cocoaView: UIView {
-
-      }
+    import AppKit
+  #elseif canImport(UIKit)
+    import UIKit
   #endif
-}
+
+  import SDGInterfaceBasics
+
+  extension Colour: View {
+
+    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+      @available(macOS 10.15, iOS 13, tvOS 13, *)
+      public var swiftUIView: AnyView {
+        return AnyView(SwiftUI.Color(self))
+      }
+    #endif
+
+    #if canImport(AppKit)
+      public var cocoaView: NSView {
+        return ColourContainer(self)
+      }
+    #else
+      public var cocoaView: UIView {
+        let view = UIView()
+        view.backgroundColor = self.uiColor
+        return view
+      }
+    #endif
+  }
+
+#endif
