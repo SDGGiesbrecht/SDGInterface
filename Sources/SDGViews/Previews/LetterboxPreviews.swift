719c1a5850e77baef6d9126af34697f47bf71c44
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-01-12 20:57:39 -0600

    Converted previews.

--- Sources/SDGViews/Previews/LetterboxPreviews.swift
+++ Sources/SDGViews/Previews/LetterboxPreviews.swift
@@ -12,51 +12,37 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-// #workaround(watchOS disabled only until Letterbox is converted to SwiftUI.)
-#if canImport(SwiftUI) && !(os(iOS) && arch(arm)) && !os(watchOS)
+#if canImport(SwiftUI) && !(os(iOS) && arch(arm))
   import SwiftUI
 
   @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
   internal struct LetterboxPreviews: SwiftUI.View {
 
-    /// Previews in one mode.
-    func preview(width: CGFloat, height: CGFloat, legacyMode: Bool) -> some SwiftUI.View {
-      let anyView: AnyView
-      if legacyMode {
-        let letterbox = Letterbox(content: Ellipse(), aspectRatio: 1)
-        letterbox.colour = .red
-        anyView = AnyView(letterbox.swiftUIView)
-      } else {
-        let letterbox = Ellipse()
-          .aspectRatio(1 as Double, contentMode: .fit)
-          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .centre)
-          .swiftUIView
-          // #workaround(Backport this.)
-          .background(Color.red)
-        anyView = AnyView(letterbox)
-      }
-      return
-        anyView
-        .frame(width: width, height: height)
-        .padding(1)
-        .border(Color.gray, width: 1)
-    }
-
-    /// Previews in both modes side‐by‐side.
-    func preview(width: CGFloat, height: CGFloat, name: String) -> some SwiftUI.View {
-      let stack = HStack(spacing: 8) {
-        preview(width: width, height: height, legacyMode: false)
-        preview(width: width, height: height, legacyMode: true)
-      }
-      return
-        stack
-        .previewDisplayName(name)
-    }
-
     internal var body: some SwiftUI.View {
+
+      func letterbox() -> AnyView {
+        return AnyView(
+          Ellipse().letterboxed(aspectRatio: 1, background: Color.red).swiftUIView
+        )
+      }
+
       return Group {
-        preview(width: 128, height: 64, name: "Pillarbox")
-        preview(width: 64, height: 128, name: "Letterbox")
+
+        previewBothModes(
+          letterbox()
+            .frame(width: 128, height: 64)
+            .padding(1)
+            .border(Color.gray, width: 1),
+          name: "Pillarbox"
+        )
+
+        previewBothModes(
+          letterbox()
+            .frame(width: 64, height: 128)
+            .padding(1)
+            .border(Color.gray, width: 1),
+          name: "Letterbox"
+        )
       }
     }
   }
