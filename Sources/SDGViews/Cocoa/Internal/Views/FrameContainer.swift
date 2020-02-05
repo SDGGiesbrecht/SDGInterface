7834765412e024ffed2b23d9ce7fbb158ec67291
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-04 21:00:21 -0600

    Refactored layout constraint.

--- Sources/SDGViews/Cocoa/Internal/Views/FrameContainer.swift
+++ Sources/SDGViews/Cocoa/Internal/Views/FrameContainer.swift
@@ -131,7 +131,7 @@
       )
     }
 
-    internal static let fillingPriority = LayoutConstraintPriority(rawValue: 255)
+    internal static let fillingPriority = CocoaLayoutConstraintPriority(rawValue: 255)
     private func preferEqual(_ attribute: NSLayoutConstraint.Attribute) {
       let constraint = NSLayoutConstraint(
         item: contents.cocoaView,
