71a516f12d453f323717fea751362a4ea19a65f4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-13 22:34:06 -0600

    Updated Workspace.

--- Workspace.swift
+++ Workspace.swift
@@ -63,3 +63,4 @@ configuration.documentation.api.ignoredDependencies = [
 
 // #workaround(SDGCornerstone 4.0.0, Does not support Windows yet.)
 configuration.supportedPlatforms.remove(.windows)
+configuration.supportedPlatforms.remove(.android)
