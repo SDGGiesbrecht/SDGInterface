aa57fc534846281262b90e5ec7aecc7fc83f8179
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-12 22:51:04 -0600

    Fixed documentation.

--- Sources/SDGViews/Cocoa/LayoutConstraintPriority.swift
+++ Sources/SDGViews/Cocoa/LayoutConstraintPriority.swift
@@ -20,7 +20,7 @@
     import UIKit
   #endif
 
-  // A layout constraint priority.
+  /// A layout constraint priority.
   public struct LayoutConstraintPriority {
 
     // MARK: - Static Properties
