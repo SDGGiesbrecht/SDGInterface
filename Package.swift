9bb9c0671635777e34b7ac5484955711054f39d3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-15 22:35:18 -0600

    Disabled resources.

--- Package.swift
+++ Package.swift
@@ -596,3 +596,10 @@ if firstEntry.hasSuffix("/Contents/Developer/usr/bin") {
   settings.append(.define("MANIFEST_LOADED_BY_XCODE"))
   sdgXCTestUtilities.swiftSettings = settings
 }
+
+if ProcessInfo.processInfo.environment["TARGETING_WEB"] == "true" {
+  for target in package.targets {
+    // #workaround(Swift 5.1.5, Web lacks foundation.)
+    target.exclude.append("Resources.swift")
+  }
+}
