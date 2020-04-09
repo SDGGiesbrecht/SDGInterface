7c1a2e3ee9114648166d465a9c5b033a31cacb96
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-09 16:12:09 -0600

    Tried @objc on Windows.

--- Sources/SDGInterfaceBasics/TextDisplayResponder.swift
+++ Sources/SDGInterfaceBasics/TextDisplayResponder.swift
@@ -12,7 +12,6 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if !os(Windows)  // #workaround(workspace version 0.32.0, Windows trips over “@objc”?)
   // #workaround(Swift 5.2, Web doesn’t have Foundation yet.)
   #if !os(WASI)
     import Foundation
@@ -28,4 +27,3 @@
       @objc func showCharacterInformation(_ sender: Any?)
     }
   #endif
-#endif
