66490fdc198e96a610bda785a846d48af5486897
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-04-09 20:47:01 -0600

    Removed workaround.

--- Sources/SDGInterfaceBasics/TextEditingResponder.swift
+++ Sources/SDGInterfaceBasics/TextEditingResponder.swift
@@ -12,7 +12,6 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if !os(Windows)  // #workaround(workspace version 0.32.0, Windows trips over “@objc”?)
   // #workaround(Swift 5.2, Web doesn’t have Foundation yet.)
   #if !os(WASI)
     import Foundation
@@ -27,4 +26,3 @@
       @objc func normalizeText(_ sender: Any?)
     }
   #endif
-#endif
