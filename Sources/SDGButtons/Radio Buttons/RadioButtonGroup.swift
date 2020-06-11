64d027ae025f5d98daba02225fe12e4ab281c9a8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-10 19:41:40 -0600

    Hooked up Cocoa implementation.

--- Sources/SDGButtons/Radio Buttons/RadioButtonGroup.swift
+++ Sources/SDGButtons/Radio Buttons/RadioButtonGroup.swift
@@ -59,7 +59,7 @@
     #if canImport(AppKit) || (canImport(UIKit) && !os(watchOS))
       public func cocoa() -> CocoaView {
         return useSwiftUIOrFallback(to: {
-          return CocoaView(CocoaImplementation(label: label, action: action))
+          return CocoaView(CocoaImplementation(labels: labels, selection: selection))
         })
       }
     #endif
@@ -78,64 +78,6 @@
           localization: LocalizationSetting.current
         )
       }
-    #endif
-  }
-#endif
-
-#warning("Move.")
-#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
-  #if canImport(AppKit)
-    import AppKit
-  #endif
-  #if canImport(UIKit)
-    import UIKit
-  #endif
-
-  import SDGText
-  import SDGLocalization
-
-  import SDGViews
-  import SDGTextDisplay
-
-  /// A set of radio buttons.
-  public class RadioButtonSet<Option, L>: AnyRadioButtonSet, CocoaViewImplementation, SDGViews.View
-  where Option: CaseIterable, L: Localization, Option.AllCases == [Option] {
-
-    // MARK: - Refreshing
-
-    public func _refreshBindings() {
-      let cases = Option.allCases
-      for index in cases.indices {
-        switch labels(cases[index]).resolved() {
-        case .text(let label):
-          #if canImport(AppKit)
-            specificCocoaView.setLabel(String(label), forSegment: index)
-          #else
-            specificCocoaView.setTitle(String(label), forSegmentAt: index)
-          #endif
-        case .symbol(let image):
-          #if canImport(AppKit)
-            specificCocoaView.setImage(image.cocoa, forSegment: index)
-            specificCocoaView.setImageScaling(.scaleProportionallyDown, forSegment: index)
-          #else
-            specificCocoaView.setImage(image.cocoa, forSegmentAt: index)
-          #endif
-        }
-      }
-    }
-
-    // MARK: - LegacyView
-
-    public func cocoa() -> CocoaView {
-      return CocoaView(specificCocoaView)
-    }
-
-    // MARK: - SpecificView
-
-    #if canImport(AppKit)
-      public let specificCocoaView: NSSegmentedControl
-    #elseif canImport(UIKit)
-      public let specificCocoaView: UISegmentedControl
     #endif
   }
 #endif
