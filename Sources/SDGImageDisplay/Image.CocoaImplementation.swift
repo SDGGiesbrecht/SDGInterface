4c6ea078e4b22f87bb38fda90ce9811ae1245043
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-16 15:49:00 -0600

    Adjusted Cocoa implementation.

---
+++ Sources/SDGImageDisplay/Image.CocoaImplementation.swift
@@ -0,0 +1,55 @@
+/*
+ Image.CocoaImplementation.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2019–2020 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
+  #if canImport(AppKit)
+    import AppKit
+  #endif
+  #if canImport(UIKit)
+    import UIKit
+  #endif
+
+  extension Image {
+
+    #if canImport(AppKit)
+      internal typealias Superclass = NSImageView
+    #else
+      internal typealias Superclass = UIImageView
+    #endif
+    internal class ImageView: Superclass {
+
+      // MARK: - Initialization
+
+      internal init(image: Image) {
+        super.init(frame: .zero)
+        self.image = image.cocoa
+
+        #if canImport(AppKit)
+          setContentCompressionResistancePriority(
+            .windowSizeStayPut,
+            for: .horizontal
+          )
+          setContentCompressionResistancePriority(
+            .windowSizeStayPut,
+            for: .vertical
+          )
+        #endif
+      }
+
+      internal required init?(coder: NSCoder) {  // @exempt(from: tests)
+        return nil
+      }
+    }
+  }
+#endif
