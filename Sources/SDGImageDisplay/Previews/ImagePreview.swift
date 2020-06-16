1b98fd4609ea4c6a28ae04b02a8f8f0114654ce5
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-16 17:15:24 -0600

    Created preview.

---
+++ Sources/SDGImageDisplay/Previews/ImagePreview.swift
@@ -0,0 +1,36 @@
+/*
+ ImagePreview.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+  import SwiftUI
+
+  import SDGViews
+
+  @available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *)
+  internal struct ImagePreviews: SwiftUI.View {
+
+    internal var body: some SwiftUI.View {
+
+      return Group {
+
+        previewBothModes(
+          Image.goRight
+            .adjustForLegacyMode()
+            .padding(),
+          name: "Image"
+        )
+      }
+    }
+  }
+#endif
