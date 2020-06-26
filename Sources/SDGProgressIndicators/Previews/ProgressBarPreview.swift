9d569fb375a16e5e2cb447ddb7c95afb991be5f7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-26 17:20:26 -0600

    Added preview.

---
+++ Sources/SDGProgressIndicators/Previews/ProgressBarPreview.swift
@@ -0,0 +1,40 @@
+/*
+ ProgressBarPreview.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2020 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+  import SwiftUI
+
+  import SDGControlFlow
+
+  import SDGViews
+
+  import SDGInterfaceLocalizations
+
+  @available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *)
+  internal struct ProgressBarPreviews: SwiftUI.View {
+
+    internal var body: some SwiftUI.View {
+
+      return Group {
+        ProgressBar(range: Shared(0...10), value: Shared(5))
+          .swiftUI()
+          .previewDisplayName("Half")
+
+        ProgressBar(range: Shared(0...10), value: Shared(nil))
+          .swiftUI()
+          .previewDisplayName("Indeterminate")
+      }
+    }
+  }
+#endif
