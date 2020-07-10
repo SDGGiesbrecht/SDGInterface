4f03b25ed1e118a718e53e0558c0ddc79045d722
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-07-10 14:44:51 -0600

    Fixed Windows.

--- Tests/SDGWindowsTests/CocoaExample.swift
+++ Tests/SDGWindowsTests/CocoaExample.swift
@@ -12,6 +12,8 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-import SDGWindows
+#if canImport(AppKit) || canImport(UIKit)
+  import SDGWindows
 
-class CocoaExample: CocoaWindow.NativeType, CocoaWindowImplementation {}
+  class CocoaExample: CocoaWindow.NativeType, CocoaWindowImplementation {}
+#endif
