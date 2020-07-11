36352e4fff6dcf6763447e401d91d5218e3c7ed1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-07-10 20:00:02 -0600

    Implemented Cocoa for macOS.

--- Sources/SDGPopOvers/PopOver.CocoaImplementation/PopOver.CocoaImplementation.swift
+++ Sources/SDGPopOvers/PopOver.CocoaImplementation/PopOver.CocoaImplementation.swift
@@ -61,6 +61,60 @@
       private let arrowEdge: SDGInterfaceBasics.Edge
       private let content: () -> Content
 
+      // MARK: - Pop Over
+
+      private func showPopOver() {
+        let popOverView = CocoaPopOverView(view: content())
+
+        let attachmentRectangle: CGRect
+        switch attachmentAnchor {
+        case .point(let point):
+          attachmentRectangle = CGRect(origin: CGPoint(point), size: CGSize(width: 0, height: 0))
+        case .rectangle(let rectangle):
+          switch rectangle {
+          case .bounds:
+            attachmentRectangle = anchor.native.frame
+          case .rectangle(let rectangle):
+            attachmentRectangle = CGRect(rectangle)
+          }
+        }
+
+        #if canImport(UIKit)
+          #warning("Audit.")
+          let controller = UIViewController()
+          #if os(tvOS)
+            controller.modalPresentationStyle = .overCurrentContext
+          #else
+            controller.modalPresentationStyle = .popover
+          #endif
+          controller.view = popOverView
+
+          let popOver = controller.popoverPresentationController
+          #if !os(tvOS)
+            popOver?.delegate = UIPopoverPresentationControllerDelegate.delegate
+          #endif
+          popOver?.sourceView = cocoa().native
+          popOver?.sourceRect =
+            sourceRectangle.map({ CGRect($0) })  // @exempt(from: tests) tvOS quirk.
+            ?? cocoa().native.frame  // @exempt(from: tests)
+          popOver?.permittedArrowDirections = .any
+
+          cocoa().native.controller?.present(controller, animated: true, completion: nil)
+        #else
+          let controller = NSViewController()
+          controller.view = popOverView
+
+          let popOver = NSPopover()
+          popOver.contentViewController = controller
+          popOver.behavior = .transient
+          popOver.show(
+            relativeTo: attachmentRectangle,
+            of: anchor.native,
+            preferredEdge: NSRectEdge(arrowEdge)
+          )
+        #endif
+      }
+
       // MARK: - SharedValueObserver
 
       internal func valueChanged(for identifier: String) {
