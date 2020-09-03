6ed9a1cb3740d78ca1bfb3139ce5fb09d6c323b3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-09-03 16:32:02 -0600

    Fixed iOS build.

--- Sources/SDGTextDisplay/Text Field/TextField.SwiftUIImplementation.swift
+++ Sources/SDGTextDisplay/Text Field/TextField.SwiftUIImplementation.swift
@@ -12,7 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if canImport(SwiftUI)
+#if canImport(SwiftUI) && !(os(iOS) && arch(arm))
   import SwiftUI
 
   import SDGControlFlow
