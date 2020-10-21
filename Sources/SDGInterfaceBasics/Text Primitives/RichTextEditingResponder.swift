6fb7e6f49ccda4cda65923c80e361d4b0420d6e6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-21 17:02:14 -0600

    Updated dependencies.

--- Sources/SDGInterfaceBasics/Text Primitives/RichTextEditingResponder.swift
+++ Sources/SDGInterfaceBasics/Text Primitives/RichTextEditingResponder.swift
@@ -12,7 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if !os(Windows)  // #workaround(Swift 5.2.4, Causes linker error.)
+#if !os(Windows)  // #warning(Swift 5.3, Causes linker error.)
   // #workaround(Swift 5.3, Web doesn’t have Foundation yet.)
   #if !os(WASI)
     import Foundation
