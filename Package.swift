103c95369a1646d7a957ce21503713ae1c8a8811
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-11-18 21:33:37 -0600

    Enabled resources.

--- Package.swift
+++ Package.swift
@@ -683,13 +683,6 @@ let package = Package(
   ]
 )
 
-if ProcessInfo.processInfo.environment["TARGETING_WEB"] == "true" {
-  for target in package.targets {
-    // #workaround(Swift 5.3, Web doesn’t have Foundation yet.)
-    target.exclude.append("Resources.swift")
-  }
-}
-
 if ProcessInfo.processInfo.environment["TARGETING_WATCHOS"] == "true" {
   // #workaround(xcodebuild -version 12.1, Test targets don’t work on watchOS.) @exempt(from: unicode)
   package.targets.removeAll(where: { $0.isTest })
