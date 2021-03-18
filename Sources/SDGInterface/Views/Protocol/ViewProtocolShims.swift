42ce9cde4161ad71a7f63594b2567d19d8caa3c1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-03-18 16:59:21 -0600

    Enabled views.

--- Sources/SDGInterface/Views/Protocol/ViewProtocolShims.swift
+++ Sources/SDGInterface/Views/Protocol/ViewProtocolShims.swift
@@ -12,29 +12,27 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if canImport(SwiftUI) || canImport(AppKit) || canImport(UIKit)
-  #if canImport(SwiftUI)
-    import SwiftUI
-  #endif
-
-  /// An implementation detail of the `View` protocol.
-  ///
-  /// This protocol is only part of the API because the compiler requires it to be referred to by name when conditionally conforming to `View`. Aside from this, you should never need to use it.
-  @available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *)
-  public protocol ViewShims {
-
-    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
-      func _swiftUIImplementation() -> SwiftUI.AnyView
-    #endif
-  }
-
-  @available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *)
-  extension ViewShims where Self: View {
-
-    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
-      public func _swiftUIImplementation() -> SwiftUI.AnyView {
-        return SwiftUI.AnyView(swiftUI())
-      }
-    #endif
-  }
+#if canImport(SwiftUI)
+  import SwiftUI
 #endif
+
+/// An implementation detail of the `View` protocol.
+///
+/// This protocol is only part of the API because the compiler requires it to be referred to by name when conditionally conforming to `View`. Aside from this, you should never need to use it.
+@available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *)
+public protocol ViewShims {
+
+  #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+    func _swiftUIImplementation() -> SwiftUI.AnyView
+  #endif
+}
+
+@available(macOS 10.15, tvOS 13, iOS 13, watchOS 6, *)
+extension ViewShims where Self: View {
+
+  #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+    public func _swiftUIImplementation() -> SwiftUI.AnyView {
+      return SwiftUI.AnyView(swiftUI())
+    }
+  #endif
+}
