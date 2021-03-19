512cb02f22bc3e6712bf9fe4631f0a757d99af8d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-03-19 10:28:19 -0600

    Enabled generic label.

--- Sources/SDGInterface/Text/Label/GenericLabel.swift
+++ Sources/SDGInterface/Text/Label/GenericLabel.swift
@@ -12,80 +12,78 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if canImport(SwiftUI) || canImport(AppKit) || canImport(UIKit)
-  #if canImport(SwiftUI)
-    import SwiftUI
-  #endif
-  #if canImport(AppKit)
-    import AppKit
-  #endif
-  #if canImport(UIKit)
-    import UIKit
-  #endif
+#if canImport(SwiftUI)
+  import SwiftUI
+#endif
+#if canImport(AppKit)
+  import AppKit
+#endif
+#if canImport(UIKit)
+  import UIKit
+#endif
 
-  import SDGControlFlow
-  import SDGText
-  import SDGLocalization
+import SDGControlFlow
+import SDGText
+import SDGLocalization
 
-  @available(watchOS 6, *)
-  internal struct GenericLabel<L, S>: LegacyView where L: Localization, S: StringFamily {
+@available(watchOS 6, *)
+internal struct GenericLabel<L, S>: LegacyView where L: Localization, S: StringFamily {
 
-    // MARK: - Initialization
+  // MARK: - Initialization
 
-    internal init(
-      _ text: Shared<UserFacing<S, L>>,
-      colour: Colour
-    ) {
-      #if DEBUG
-        _ = text.value.resolved()  // Eager execution to simplify testing.
-      #endif
-      self.text = text
-      self.colour = colour
-    }
+  internal init(
+    _ text: Shared<UserFacing<S, L>>,
+    colour: Colour
+  ) {
+    #if DEBUG
+      _ = text.value.resolved()  // Eager execution to simplify testing.
+    #endif
+    self.text = text
+    self.colour = colour
+  }
 
-    // MARK: - Properties
+  // MARK: - Properties
 
-    private let text: Shared<UserFacing<S, L>>
-    private let colour: Colour
+  private let text: Shared<UserFacing<S, L>>
+  private let colour: Colour
 
-    // MARK: - LegacyView
+  // MARK: - LegacyView
 
-    #if canImport(AppKit) || (canImport(UIKit) && !os(watchOS))
-      public func cocoa() -> CocoaView {
-        // #workaround(Swift 5.3.2, SwiftUI would be a step backward from AppKit or UIKit without the ability to interact properly with menus such as “Copy”.)
-        #if !canImport(AppKit) && !canImport(UIKit)
-          return useSwiftUIOrFallback(to: {
-            return CocoaView(
-              CocoaImplementation(text: text, colour: colour)
-            )
-          })
-        #else
+  #if canImport(AppKit) || (canImport(UIKit) && !os(watchOS))
+    public func cocoa() -> CocoaView {
+      // #workaround(Swift 5.3.2, SwiftUI would be a step backward from AppKit or UIKit without the ability to interact properly with menus such as “Copy”.)
+      #if !canImport(AppKit) && !canImport(UIKit)
+        return useSwiftUIOrFallback(to: {
           return CocoaView(
             CocoaImplementation(text: text, colour: colour)
           )
-        #endif
+        })
+      #else
+        return CocoaView(
+          CocoaImplementation(text: text, colour: colour)
+        )
+      #endif
+    }
+  #endif
+}
+
+// #workaround(Swift 5.3.2, SwiftUI would be a step backward from AppKit or UIKit without the ability to interact properly with menus such as “Copy”.)
+#if !canImport(AppKit) && !(canImport(UIKit) && !os(watchOS))
+  @available(watchOS 6, *)
+  extension GenericLabel: View {
+
+    // MARK: - View
+
+    #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
+      public func swiftUI() -> some SwiftUI.View {
+        return SwiftUIImplementation(
+          text: text,
+          colour: SwiftUI.Color(colour),
+          localization: LocalizationSetting.current
+        )
       }
     #endif
   }
-
-  // #workaround(Swift 5.3.2, SwiftUI would be a step backward from AppKit or UIKit without the ability to interact properly with menus such as “Copy”.)
-  #if !canImport(AppKit) && !(canImport(UIKit) && !os(watchOS))
-    @available(watchOS 6, *)
-    extension GenericLabel: View {
-
-      // MARK: - View
-
-      #if canImport(SwiftUI) && !(os(iOS) && arch(arm))
-        public func swiftUI() -> some SwiftUI.View {
-          return SwiftUIImplementation(
-            text: text,
-            colour: SwiftUI.Color(colour),
-            localization: LocalizationSetting.current
-          )
-        }
-      #endif
-    }
-  #else
-    extension GenericLabel: CocoaViewImplementation {}
-  #endif
+#else
+  extension GenericLabel: CocoaViewImplementation {}
 #endif
