afa717676c75459f0fd46a61926eb31a64be8f5b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-04-16 18:14:50 -0600

    Refreshed.

--- Sources/SDGInterface/Text/Log/DerivedLog.CocoaImplementation.swift
+++ Sources/SDGInterface/Text/Log/DerivedLog.CocoaImplementation.swift
@@ -1,3 +1,17 @@
+/*
+ DerivedLog.CocoaImplementation.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2021 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
 #if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
   #if canImport(AppKit)
     import AppKit
@@ -23,7 +37,7 @@
       ) {
         self.source = source
         defer { source.register(observer: self) }
-        
+
         self.derivation = derivation
 
         super.init(
