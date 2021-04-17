2d2635d1860f3cb4b70150ad4033f2a6479dfba2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-04-16 20:59:01 -0600

    Added derived log.

---
+++ Sources/SDGInterface/Text/Log/DerivedLog.CocoaImplementation.swift
@@ -0,0 +1,77 @@
+/*
+ DerivedLog.CocoaImplementation.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2021 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
+  #if canImport(AppKit)
+    import AppKit
+  #endif
+  #if canImport(UIKit)
+    import UIKit
+  #endif
+
+  import SDGControlFlow
+  import SDGLogic
+
+  extension DerivedLog {
+
+    internal class CocoaImplementation: TextEditor.CocoaFrameView, SharedValueObserver {
+
+      // MARK: - Initialization
+
+      internal init(
+        source: Shared<Source>,
+        derivation: @escaping (Source) -> RichText,
+        transparentBackground: Bool,
+        logMode: Bool
+      ) {
+        self.source = source
+        defer { source.register(observer: self) }
+
+        self.derivation = derivation
+
+        super.init(
+          transparentBackground: transparentBackground,
+          logMode: logMode
+        )
+      }
+
+      internal required init?(coder: NSCoder) {  // @exempt(from: tests)
+        return nil
+      }
+
+      // MARK: - Properties
+
+      private let source: Shared<Source>
+      private let derivation: (Source) -> RichText
+
+      // MARK: - SharedValueObserver
+
+      internal func valueChanged(for identifier: String) {
+        let derived = derivation(source.value)
+        let newValue = derived.attributedString()
+        let existingValue: NSAttributedString
+        #if canImport(AppKit)
+          existingValue = textView.attributedString()
+        #else
+          existingValue =
+            textView.attributedText
+            ?? NSAttributedString()  // @exempt(from: tests) Never nil.
+        #endif
+        if existingValue ≠ newValue {
+          updateContents(to: derived)
+        }
+      }
+    }
+  }
+#endif
---
+++ Sources/SDGInterface/Text/Log/DerivedLog.swift
@@ -0,0 +1,70 @@
+/*
+ DerivedLog.swift
+
+ This source file is part of the SDGInterface open source project.
+ https://sdggiesbrecht.github.io/SDGInterface
+
+ Copyright ©2021 Jeremy David Giesbrecht and the SDGInterface project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+#if (canImport(AppKit) || canImport(UIKit)) && !os(watchOS)
+  #if canImport(AppKit)
+    import AppKit
+  #endif
+  #if canImport(UIKit)
+    import UIKit
+  #endif
+
+  import SDGControlFlow
+
+  /// A textual log derived from other data.
+  public struct DerivedLog<Source>: CocoaViewImplementation {
+
+    // MARK: - Initialization
+
+    /// Creates a log.
+    ///
+    /// - Parameters:
+    ///   - source: The source of the data.
+    ///   - derivation: A closure which derives the log from the data.
+    ///   - sourceValue: The source value from which to derive the log.
+    ///   - transparentBackground: Optional. Pass `true` to make the background transparent.
+    public init(
+      source: Shared<Source>,
+      derivation: @escaping (_ sourceValue: Source) -> RichText,
+      transparentBackground: Bool = false
+    ) {
+      self.source = source
+      self.derivation = derivation
+      self.transparentBackground = transparentBackground
+    }
+
+    // MARK: - Properties
+
+    private let source: Shared<Source>
+    private let derivation: (Source) -> RichText
+    private let transparentBackground: Bool
+
+    // MARK: - LegacyView
+
+    #if canImport(AppKit) || (canImport(UIKit) && !os(watchOS))
+      public func cocoa() -> CocoaView {
+        let cocoa = DerivedLog.CocoaImplementation(
+          source: source,
+          derivation: derivation,
+          transparentBackground: transparentBackground,
+          logMode: true
+        )
+        #if !os(tvOS)
+          cocoa.setEditability(false)
+        #endif
+        return CocoaView(cocoa)
+      }
+    #endif
+  }
+#endif
