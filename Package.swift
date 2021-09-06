709ba00909b343bd08fa33f6e7dc9405e508dcbe
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-06 16:10:55 -0600

    Updated SDGCornerstone.

--- Package.swift
+++ Package.swift
@@ -162,7 +162,7 @@ let package = Package(
     .library(name: "_SDGInterfaceSample", targets: ["SDGInterfaceSample"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", from: Version(7, 1, 3))
+    .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", from: Version(7, 2, 3))
   ],
   targets: [
 
