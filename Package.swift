8921d900a5ebfe1c8589f3b25f9aecb4732fd8e2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-11-02 15:08:21 -0600

    Removed test case workaround.

--- Package.swift
+++ Package.swift
@@ -346,9 +346,5 @@ for target in package.targets {
     .define("PLATFORM_LACKS_FOUNDATION_PROCESS_INFO", .when(platforms: [.wasi])),
     .define("PLATFORM_LACKS_FOUNDATION_RUN_LOOP", .when(platforms: [.wasi])),
     // @endExample
-
-    // Internal:
-    // #workaround(SDGCornerstone 7.2.4, Web lacks TestCase.)
-    .define("PLATFORM_LACKS_SDG_CORNERSTONE_TEST_CASE", .when(platforms: [.watchOS])),
   ])
 }
