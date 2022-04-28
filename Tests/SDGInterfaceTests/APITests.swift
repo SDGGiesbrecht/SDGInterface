fa4e84ee789c9121f9d2ce68011ecce251995223
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-04-27 23:17:49 -0600

    a..

--- Tests/SDGInterfaceTests/APITests.swift
+++ Tests/SDGInterfaceTests/APITests.swift
@@ -659,8 +659,8 @@ final class APITests: ApplicationTestCase {
 
   func testLegacyView() {
     if #available(watchOS 6, *) {
-      // #warning(Did adding Android fix it?)
-      #if os(watchOS) && !os(Android)
+      // #warning(Did adding Linux & Android fix it?)
+      #if os(watchOS) && !os(Linux) && !os(Android)
         class Legacy: LegacyView, SDGInterface.View {
           func swiftUI() -> some SwiftUI.View {
             return EmptyView().swiftUI()
